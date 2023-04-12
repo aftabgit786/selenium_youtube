43d50a880b63ac696e5ef61ff2f2d096d14ae401
aftabgit786 <gb485585@gmail.com>
2023-04-12 14:14:55 +0500

    modify code with upload_time and add csv file with channel url

--- t2.py
+++ t2.py
@@ -16,6 +16,6 @@ soup = BeautifulSoup(html, 'html.parser')
 data = extract_videos_data(soup)
 
 df = pd.DataFrame(data)
-df.to_csv('youtube_data.csv', index=False)
+df.to_csv('zusmani78.csv', index=False)
 
 print("Data successfully saved to CSV file.")
--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ def extract_videos_data(soup):
     for videos in video_elements:
         title = videos.find('yt-formatted-string', {"id": "video-title"}).text
         views = videos.find('span', {"class": "inline-metadata-item style-scope ytd-video-meta-block"}).text
-        upload_time = videos.find('span', {"class": "inline-metadata-item style-scope ytd-video-meta-block"}).next
+        upload_time = videos.find('div', {"id": "metadata-line"}).text.split('\n')[-3]
         video_duration = videos.find('span', {"class": "style-scope ytd-thumbnail-overlay-time-status-renderer"}).text.strip()
         thumbnails = videos.find('img', {"style": "background-color: transparent;"}).get('src')
         video_data = {
